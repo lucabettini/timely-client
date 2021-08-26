6528621713ade9d86a03b527861920f3dac3f19b
lucabettini <bettiniluca1@gmail.com>
2021-08-26 20:03:26 +0200

    Finished setup with Material UI

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import { CssBaseline } from '@material-ui/core';
+import React from 'react';
+import { Route, Router } from 'react-router-dom';
 import './App.css';
+import Login from './components/auth/Login';
 
 function App() {
-  return <></>;
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <Router>
+        <Route exact='/login'>
+          <Login />
+        </Route>
+      </Router>
+    </React.Fragment>
+  );
 }
 
 export default App;
