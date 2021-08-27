24b52931efe0cd201d79570d4fc41e2b52219e38
lucabettini <bettiniluca1@gmail.com>
2021-08-27 18:54:30 +0200

    Styled Login component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import { CssBaseline } from '@material-ui/core';
+import { CssBaseline, createTheme, ThemeProvider } from '@material-ui/core';
 import React from 'react';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import './App.css';
 import Login from './components/auth/Login';
 
+const theme = createTheme({
+  palette: {
+    error: { main: '#FF773D' },
+    success: { main: '#DDFFD9' },
+    warning: { main: '#FDC31D' },
+    primary: { main: '#28b5f4' },
+  },
+});
+
 function App() {
   return (
-    <Router>
-      <CssBaseline />
-      {/* <Container maxWidth='sm'> */}
-      <Switch>
-        <Route exact path='/'>
-          <div>Slash</div>
-        </Route>
-        <Route exact path='/login'>
-          <Login />
-        </Route>
-      </Switch>
-      {/* </Container> */}
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <CssBaseline />
+        <Switch>
+          <Route exact path='/'>
+            <div>Slash</div>
+          </Route>
+          <Route exact path='/login'>
+            <Login />
+          </Route>
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 }
 
--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,4 +1,5 @@
-import { Button, Grid } from '@material-ui/core';
+import { Button, Grid, Paper, useTheme } from '@material-ui/core';
+import { makeStyles } from '@material-ui/styles';
 import axios from 'axios';
 import React from 'react';
 import * as yup from 'yup';
@@ -11,6 +12,7 @@ const Login = () => {
     password: '',
   });
 
+  // Validation schema
   const schema = {
     email: yup.string().email().required(),
     password: yup
@@ -19,19 +21,10 @@ const Login = () => {
       .required(),
   };
 
-  const inputProps = (name) => {
-    return {
-      name,
-      label: name.charAt(0).toUpperCase() + name.substring(1),
-      handleChange: form.onChange,
-      handleBlur: form.onBlur,
-      isValid: form.isValid(name),
-      value: form.values[name],
-    };
-  };
-
   const handleSubmit = async (e) => {
     e.preventDefault();
+
+    // Set as touched and run validation on all inputs
     form.onSubmit(schema);
 
     if (form.canSubmit()) {
@@ -48,40 +41,76 @@ const Login = () => {
     }
   };
 
+  // INPUT PROPS
+  const inputProps = (name) => {
+    return {
+      name,
+      label: name.charAt(0).toUpperCase() + name.substring(1),
+      handleChange: form.onChange,
+      handleBlur: form.onBlur,
+      isValid: form.isValid(name),
+      value: form.values[name],
+    };
+  };
+
+  // CSS STYLING
+  const theme = useTheme();
+
+  const useStyles = makeStyles({
+    container: {
+      minHeight: '100vh',
+      paddingBottom: '20vh',
+      flexGrow: 1,
+    },
+    form: {
+      border: `1px solid ${theme.palette.primary.main}`,
+      padding: '5%',
+      backgroundColor: `${theme.palette.success.main}`,
+    },
+  });
+
+  console.log(theme);
+
+  const classes = useStyles();
+
   return (
     <Grid
       container
-      spacing={0}
-      direction='column'
       alignItems='center'
       justifyContent='center'
-      style={{
-        minHeight: '100vh',
-        backgroundColor: 'cyan',
-        paddingBottom: '20%',
-      }}
+      className={classes.container}
     >
-      <Grid item xs={6} style={{ border: '1px solid blue' }}>
-        <form
-          noValidate
-          autoComplete='off'
-          style={{ display: 'flex', flexDirection: 'column', padding: '20px' }}
-          onSubmit={handleSubmit}
-        >
-          <CustomInput schema={schema.email} {...inputProps('email')} />
-          <CustomInput
-            schema={schema.password}
-            type='password'
-            {...inputProps('password')}
-          />
-          <Button
-            type='submit'
-            variant='contained'
-            color='primary'
-            style={{ marginTop: '10px' }}
-          >
-            INVIA
-          </Button>
+      <Grid item xs={10} sm={6} lg={3} className={classes.item}>
+        <form noValidate autoComplete='off' onSubmit={handleSubmit}>
+          <Paper className={classes.form}>
+            <Grid container direction='column' spacing={3}>
+              <Grid item xs={12}>
+                <CustomInput
+                  schema={schema.email}
+                  style={{ width: '100%' }}
+                  {...inputProps('email')}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <CustomInput
+                  schema={schema.password}
+                  type='password'
+                  style={{ width: '100%' }}
+                  {...inputProps('password')}
+                />
+              </Grid>
+              <Grid container item xs={12} justifyContent='center'>
+                <Button
+                  type='submit'
+                  variant='contained'
+                  color='primary'
+                  size='large'
+                >
+                  LOGIN
+                </Button>
+              </Grid>
+            </Grid>
+          </Paper>
         </form>
       </Grid>
     </Grid>
